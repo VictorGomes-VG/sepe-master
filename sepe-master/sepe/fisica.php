c57055181558a766b83d082f3d2ccb52135520bf
Vetor-Gomes <vitorvex@gmail.com>
2017-09-12 16:47:37 -0300

    Add files via upload

---
+++ sepe-master/sepe/fisica.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <meta charset="UTF-8">
+
+</head>
+<body background="img/fundo.jpg">
+<style>
+    @font-face {
+        font-family: airbus;
+        src: url(fontes/airbus.ttf);
+    }
+</style>
+
+<div class="row" style="border-radius: 30px; margin: 30px auto; background-color:rgba(300, 300, 300, 0.8); width: 850px">
+    <h2 align="center" style="color: black;font-size: 56px; font-family: 'airbus'"> FÍSICA !</h2>
+</div>
+<div class="container">
+    <div class="row" style="border-radius: 30px; margin: auto; background-color:rgba(50, 50, 50, 0.9);width: 850px">
+        <div class="col-md-12">
+            <div class="col-md-4">
+
+                <h3 align="center" style="color: white;font-size: 56px; font-family: 'airbus'"> FORMULARIO------------------------------------------------------------</h3>
+
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
---
+++ sepe-master/sepe/fontes/matematica.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <meta charset="UTF-8">
+    <title>Calculator 2000</title>
+</head>
+<body background="img/fundo.jpg">
+<style>
+    @font-face {
+        font-family: airbus;
+        src: url(fontes/airbus.ttf);
+    }
+</style>
+<div class="row" style="border-radius: 30px; margin: 30px auto; background-color:rgba(300, 300, 300, 0.5); width: 850px">
+    <h2 align="center" style="color: black;font-size: 101px; font-family: 'airbus'">MATEMÁTICA!</h2>
+</div>
+
+</body>
+</html>
